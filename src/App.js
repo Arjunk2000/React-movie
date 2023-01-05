421bc1adc4b2a4d8a741442f716ad83950bbf697
Arjunk2000 <96836318+Arjunk2000@users.noreply.github.com>
2023-01-05 13:46:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Search from "./Search";
+
+// const url = "https://www.omdbapi.com/?s=man&apikey=7e1cc861";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="heading">
+        <h1>HOOKED</h1>
+      </div>
+      <div className="components">
+        <Search />
+      </div>
+    </div>
+  );
+}
+
+export default App;
